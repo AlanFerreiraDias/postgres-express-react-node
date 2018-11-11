643e4c8660b02d0be1283b9986a8f8797dd98abe
Alan Dias <dias.ferreira.alan@gmail>
2018-11-11 17:05:25 -0200

    initial commit

---
+++ migrations/20181109232453-create-company.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('company', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      legal_name: {
+        type: Sequelize.STRING
+      },
+      trade_name: {
+        type: Sequelize.STRING
+      },
+      legal_type: {
+        type: Sequelize.STRING
+      },
+      registration_number: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('company');
+  }
+};
---
+++ models/company.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const company = sequelize.define('company', {
+    legal_name: DataTypes.STRING,
+    trade_name: DataTypes.STRING,
+    legal_type: DataTypes.STRING,
+    registration_number: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone: DataTypes.STRING
+  }, {
+    underscored: true,
+  });
+  company.associate = function(models) {
+    // associations can be defined here
+    company.hasMany(models.phone)
+  };
+  return company;
+};
